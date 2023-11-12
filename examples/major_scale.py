b64f4549634f8e1939660df38a2275ae02fbc8db
Josh Read <joshua-read@hotmail.co.uk>
2023-11-12 09:44:08 +0000

    Use Grid class to draw fretboard

--- examples/major_scale.py
+++ examples/major_scale.py
@@ -1,41 +1,7 @@
+from grid import Grid
 from units import intervals, notes, scales
 import drawsvg as draw
 from itertools import cycle, islice, count
-
-
-class Grid:
-
-    def __init__(self, x, y, n_cols, n_rows, cell_size):
-        self.x = x
-        self.y = y
-        self.n_cols = n_cols
-        self.n_rows = n_rows
-        self.cell_width, self.cell_height = cell_size
-
-    @property
-    def col_edges(self):
-        return [n * self.cell_width + self.x for n in range(self.n_cols + 1)]
-
-    @property
-    def col_centres(self):
-        return [(n + 0.5) * self.cell_width + self.x for n in range(self.n_cols)]
-
-    @property
-    def row_edges(self):
-        return [n * self.cell_height + self.y for n in range(self.n_rows + 1)]
-
-    @property
-    def row_centres(self):
-        return [(n + 0.5) * self.cell_height + self.y for n in range(self.n_rows)]
-
-    @property
-    def grid_width(self):
-        return self.cell_width * self.n_cols
-
-    @property
-    def grid_height(self):
-        return self.cell_height * self.n_rows
-
 
 width = 400
 height = 400
--- examples/scale_drawings.py
+++ examples/scale_drawings.py
@@ -2,30 +2,37 @@ import drawsvg as draw
 import numpy as np
 
 from fretboard import Fretboard
+from grid import Grid
 from units import intervals, scales, notes
 
 
 class FretboardDrawing(draw.Drawing):
 
-    def __init__(self, fretboard, *args, **kwargs):
+    def __init__(self, fretboard, fret_size, *args, pad=15, **kwargs):
         self.fretboard = fretboard
-        super(FretboardDrawing, self).__init__(*args, **kwargs)
-        self.string_locations = np.linspace(0, self.width, len(self.fretboard.string_notes)+2)[1:-1]
-        self.fret_locations = np.linspace(0, self.height, self.fretboard.n_frets + 3)[1:-1]
-        self.fret_centres = self.fret_locations[:-1] + (self.fret_locations[1] - self.fret_locations[0]) / 2
+        fret_width, fret_height = fret_size
+        n_cols, n_rows = len(fretboard.string_notes)-1, fretboard.n_frets
+        cell_size = fret_width / n_cols, fret_height
+        self.grid = Grid(pad, pad, n_cols, n_rows, cell_size)
+        drawing_width = self.grid.grid_width + 2 * pad
+        drawing_height = self.grid.grid_height + 2 * pad
+        super(FretboardDrawing, self).__init__(drawing_width, drawing_height, *args, **kwargs)
+        self.string_locations = self.grid.col_edges
+        self.fret_locations = self.grid.row_edges
+        self.fret_centres = self.grid.row_centres
         self.draw_fretboard()
 
     def draw_fretboard(self):
         # draw strings
+        ymin, ymax = self.fret_locations[0], self.fret_locations[-1]
         for x in self.string_locations:
-            ymin, ymax = self.fret_locations[[0, -1]]
             line = draw.Lines(x, ymin,
                               x, ymax,
                               stroke='black')
             self.append(line)
         # draw frets
+        xmin, xmax = self.string_locations[0], self.string_locations[-1]
         for y in self.fret_locations:
-            xmin, xmax = self.string_locations[[0, -1]]
             line = draw.Lines(xmin, y,
                               xmax, y,
                               stroke='black')
@@ -51,13 +58,13 @@ if __name__ == '__main__':
     for note_offset, note_name in notes.items():
         for scale_name, scale_degrees in scales.items():
             fb = Fretboard(n_frets=13, string_intervals=[5, 5, 5, 4, 5], note_offset=(8 + note_offset))
-            fbd = FretboardDrawing(fb, width=500, height=1000)
+            fbd = FretboardDrawing(fb, fret_size=(6*25, 50))
             fbd.draw_notes(scale_degrees, intervals)
             fbd.save_svg(f'drawings/scales/{note_name}_{scale_name}_scale.svg')
 
     # draw major and pentatonic scales on large grid
     for name, scale in scales.items():
         fb = Fretboard(n_frets=15, n_strings=15, string_intervals=5, note_offset=6)
-        fbd = FretboardDrawing(fb, width=500, height=1000)
+        fbd = FretboardDrawing(fb, fret_size=(15*25, 50))
         fbd.draw_notes(scale, intervals)
         fbd.save_svg(f'drawings/{name}_scale.svg')
