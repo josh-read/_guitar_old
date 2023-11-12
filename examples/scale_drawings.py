e9f8e9bfe9ba715877de30163e39c07f425b38e7
Josh Read <joshua-read@hotmail.co.uk>
2023-11-12 09:48:55 +0000

    Simplify fretboard cell size initialisation

--- examples/scale_drawings.py
+++ examples/scale_drawings.py
@@ -8,11 +8,9 @@ from units import intervals, scales, notes
 
 class FretboardDrawing(draw.Drawing):
 
-    def __init__(self, fretboard, fret_size, *args, pad=15, **kwargs):
+    def __init__(self, fretboard, *args, cell_size=(25, 50), pad=15, **kwargs):
         self.fretboard = fretboard
-        fret_width, fret_height = fret_size
         n_cols, n_rows = len(fretboard.string_notes)-1, fretboard.n_frets
-        cell_size = fret_width / n_cols, fret_height
         self.grid = Grid(pad, pad, n_cols, n_rows, cell_size)
         drawing_width = self.grid.grid_width + 2 * pad
         drawing_height = self.grid.grid_height + 2 * pad
@@ -58,13 +56,13 @@ if __name__ == '__main__':
     for note_offset, note_name in notes.items():
         for scale_name, scale_degrees in scales.items():
             fb = Fretboard(n_frets=13, string_intervals=[5, 5, 5, 4, 5], note_offset=(8 + note_offset))
-            fbd = FretboardDrawing(fb, fret_size=(6*25, 50))
+            fbd = FretboardDrawing(fb)
             fbd.draw_notes(scale_degrees, intervals)
             fbd.save_svg(f'drawings/scales/{note_name}_{scale_name}_scale.svg')
 
     # draw major and pentatonic scales on large grid
     for name, scale in scales.items():
         fb = Fretboard(n_frets=15, n_strings=15, string_intervals=5, note_offset=6)
-        fbd = FretboardDrawing(fb, fret_size=(15*25, 50))
+        fbd = FretboardDrawing(fb)
         fbd.draw_notes(scale, intervals)
         fbd.save_svg(f'drawings/{name}_scale.svg')
