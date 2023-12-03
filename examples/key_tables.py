1f78800a085c96d95a2a2b0726d993d202a88915
Josh Read <joshua-read@hotmail.co.uk>
2023-12-03 20:57:20 +0000

    Add scale and note objects

--- examples/key_tables.py
+++ examples/key_tables.py
@@ -1,42 +1,38 @@
 import drawsvg as draw
-
-from units import intervals, scales
 from grid import Grid
-from itertools import count
+from notes import Scale
 
+chromatic_scale = Scale.chromatic()
+diatonic_scale = Scale.diatonic()
 
-major_scale = scales['major']
-scale_degrees = [v for k, v in intervals.items() if k in major_scale]
-interval_list = list(intervals.values())
+n_row = len(diatonic_scale) + 1
+n_col = len(chromatic_scale) + 1
 
-n_cols = len(intervals) + 1
-n_rows = len(major_scale) + 1
+grid = Grid(x=0, y=0, n_cols=n_col, n_rows=n_row, cell_size=(50, 50))
 
-# initialise grid and drawing
-grid = Grid(0, 0, n_cols, n_rows, cell_size=(25, 25))
-d = draw.Drawing(width=grid.grid_width, height=grid.grid_height)
+d = draw.Drawing(grid.grid_width, height=grid.grid_height)
 
-# draw headings in first row
-first_row_y = grid.row_centres[0]
-counter = count(start=1)
-for i, (c, l) in enumerate(zip(grid.col_centres, grid.col_edges), start=0):
-    if i-1 in major_scale:
-        n = next(counter)
-        text = draw.Text(str(n), x=c, y=first_row_y, font_size=12, text_anchor='middle', dominant_baseline='middle')
+# fill in the top row
+top_row_y = grid.row_centres[0]
+for e, c, chromatic_note in zip(grid.col_edges[1:], grid.col_centres[1:], chromatic_scale):
+    if chromatic_note in diatonic_scale:
+        degree = diatonic_scale.semitones.index(chromatic_note.semitone)
+        text = draw.Text(str(degree + 1), x=c, y=top_row_y, font_size=12, text_anchor='middle', dominant_baseline='middle')
         d.append(text)
-        if n in (1, 3, 5):
-            rect = draw.Rectangle(x=l, y=grid.y, width=grid.cell_width, height=grid.grid_height,
+        if degree in (1, 3, 5):
+            rect = draw.Rectangle(x=e, y=grid.y, width=grid.cell_width, height=grid.grid_height,
                                   fill='orange', fill_opacity=0.3, stroke='orange')
             d.append(rect)
 
 # draw in rest of table
-for scale_degree, (semitone, y) in enumerate(zip(major_scale, grid.row_centres[1:])):
-    row = interval_list[semitone:] + interval_list[:semitone]
+for offset, (semitone, y) in enumerate(zip(diatonic_scale, grid.row_centres[1:])):
+    scale = Scale.chromatic()
     # print(interval_list[semitone:], interval_list[:semitone])
-    text = draw.Text(str(scale_degree + 1), x=grid.col_centres[0], y=y, font_size=12, text_anchor='middle', dominant_baseline='middle')
+    text = draw.Text(str(offset + 1), x=grid.col_centres[0], y=y, font_size=12, text_anchor='middle', dominant_baseline='middle')
     d.append(text)
-    for interval, c in zip(row, grid.col_centres[1:]):
-        text = draw.Text(interval, x=c, y=y, font_size=12, text_anchor='middle', dominant_baseline='middle')
+    for note, c in zip(scale, grid.col_centres[1:]):
+        note.semitone += semitone.semitone
+        text = draw.Text(note.interval, x=c, y=y, font_size=12, text_anchor='middle', dominant_baseline='middle')
         d.append(text)
 
 d.save_svg('../drawings/key_table.svg')
---
+++ src/notes.py
@@ -0,0 +1,143 @@
+import drawsvg as draw
+from grid import Grid
+import seaborn as sns
+from itertools import accumulate
+
+
+
+latin_names = {
+    0: 'C',
+    1: 'C#',
+    2: 'D',
+    3: 'D#',
+    4: 'E',
+    5: 'F',
+    6: 'F#',
+    7: 'G',
+    8: 'G#',
+    9: 'A',
+    10: 'A#',
+    11: 'B',
+}
+
+
+intervals = {
+    0: 'P1',
+    1: 'm2',
+    2: 'M2',
+    3: 'm3',
+    4: 'M3',
+    5: 'P4',
+    6: 'TT',
+    7: 'P5',
+    8: 'm6',
+    9: 'M6',
+    10: 'm7',
+    11: 'M7',
+    12: 'P8'
+}
+
+
+class Scale:
+    """When working with the scale object we work with semitones."""
+
+    def __init__(self, semitones, root_note=None):
+        """Initialise with the semitones in the scale. A root note can additionally be set."""
+        self.semitones = semitones
+        self.root_note = root_note
+        self.iter_semitones = None
+
+    def __len__(self):
+        return len(self.semitones)
+
+    def __iter__(self):
+        self.iter_semitones = iter(self.semitones)
+        return self
+
+    def __next__(self):
+        try:
+            semitone = next(self.iter_semitones)
+        except StopIteration as e:
+            raise e
+        return Note(semitone, scale=self)
+
+    @classmethod
+    def chromatic(cls, *args, **kwargs):
+        chromatic_semitones = range(13)
+        return cls(semitones=chromatic_semitones, *args, **kwargs)
+
+    @classmethod
+    def diatonic(cls, *args, mode=0, **kwargs):
+        ionian_intervals = [2, 2, 1, 2, 2, 2, 1]
+        mode_intervals = ionian_intervals[mode:] + ionian_intervals[:mode]
+        mode_semitones = list(accumulate(mode_intervals, initial=0))
+        return cls(semitones=mode_semitones, *args, **kwargs)
+
+
+class Note:
+
+    def __init__(self, semitone, scale=None):
+        """Get information about the note."""
+        self.semitone = semitone
+        self.scale = scale
+
+    def __repr__(self):
+        return f"Note({self.interval=})"
+
+    def __eq__(self, other):
+        if isinstance(other, Note):
+            try:
+                return self.latin_name == other.latin_name
+            except TypeError:
+                return self.semitone == other.semitone
+        else:
+            return self.semitone == other
+
+    @property
+    def interval(self):
+        return intervals[self.semitone % 12]
+
+    @property
+    def latin_name(self):
+        if self.scale.root_note is None:
+            raise TypeError("Parent scale has no root note.")
+        return latin_names[(self.semitone + self.scale.root_note) % 12]
+
+
+# face color, stroke color
+colors = {
+    0: ('red', 'black'),
+    1: ('black', 'white'),
+    2: ('black', 'white'),
+    3: ('black', 'white'),
+    4: ('black', 'white'),
+    5: ('black', 'white'),
+    6: ('black', 'white'),
+    7: ('black', 'white'),
+    8: ('black', 'white'),
+    9: ('black', 'white'),
+    10: ('black', 'white'),
+    11: ('black', 'white'),
+    12: ('black', 'white'),
+}
+
+
+def draw_note(drawing, x, y, note):
+    face_color, stroke_color = colors[note.semitone % 12]
+    circle = draw.Circle(x, y, r=20, fill=face_color, stroke=stroke_color)
+    drawing.append(circle)
+    text = draw.Text(note.interval, font_size=12, x=x, y=y, text_anchor='middle', dominant_baseline='middle', fill=stroke_color)
+    drawing.append(text)
+
+
+if __name__ == '__main__':
+    chromatic_scale = Scale.chromatic()
+
+    grid = Grid(x=0, y=0, n_cols=len(chromatic_scale), n_rows=1, cell_size=(50, 50))
+    d = draw.Drawing(width=grid.grid_width, height=grid.grid_height)
+
+    y, = grid.row_centres
+    for x, note in zip(grid.col_centres, chromatic_scale):
+        draw_note(d, x, y, note)
+
+    d.save_svg('../drawings/notes.svg')
